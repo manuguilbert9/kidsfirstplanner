a45394d06b46e2eed7ef8f9466f648210aadf936
Manu Guilbert <manu.guilbert9@gmail.com>
2025-09-15 14:02:00 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "studio-5908953384-f26d4",
-  appId: "1:30293592997:web:86fed49420a4d70cc7b64c",
-  storageBucket: "studio-5908953384-f26d4.firebasestorage.app",
-  apiKey: "AIzaSyAjW45R_lPqD594vzmW2tGHfu2JWXvwarI",
-  authDomain: "studio-5908953384-f26d4.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "30293592997"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
